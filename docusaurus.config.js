11ec4550257eaa388ffe07efa5dd12882174de64
cyril <cyril.deom@paulwurth.com>
2021-08-12 16:33:40 +0200

    udpates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,20 +3,26 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
+  title: 'Operator manual',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon2.ico',
   organizationName: 'bfxpert', // Usually your GitHub org/user name.
   projectName: 'bfxpert', // Usually your repo name.
   themeConfig: {
+    hideableSidebar: false,
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     navbar: {
       title: '',
       logo: {
-        alt: 'BFxpert',
+        alt: 'BFXpert (Operator manual)',
         src: 'img/pw-logo.png',
       },
       items: [
